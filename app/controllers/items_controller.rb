a53d852f1a139913cc8d6cd663e44c711a53c2be
Yuhei-21 <67134329+Yuhei-21@users.noreply.github.com>
2020-08-03 19:24:58 +0900

    exhibition

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,39 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+    @category = Category.all
+    @condition = Condition.all
+    @postagepayer = Postagepayer.all
+    @postagetype = Postagetype.all
+    @preparationday = Preparationday.all
+  end
+
+  def create
+    @item = Item.create(item_params)
+    redirect_to @item
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id]) 
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update params.require(:item).permit(item_params)
+    redirect_to @item
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image,:name,:item_explanation, :price, :category_id, :condition_id, :preparationday_id, :postagetype_id, :postagepayer_id).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :preparationday
+  belongs_to_active_hash :postagetype
+  belongs_to_active_hash :postagepayer
+  has_one_attached :image
 end
