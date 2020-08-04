4dd7b5e6653275c53f562f9f71737467d184d0c1
Yuhei-21 <67134329+Yuhei-21@users.noreply.github.com>
2020-08-04 12:22:40 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,6 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   validates_inclusion_of :price, in: 300..100000000
+  validates :image_presence
+  alidates :name, :text, :category_id, :condition_id, :preparationday_id, :postagetype_id, :postagepayer_id,:user_id, presence: true
 end
