a17e519db70bb8646ee23b3f61d4f083b6061b94
Yuhei-21 <67134329+Yuhei-21@users.noreply.github.com>
2020-08-04 16:42:06 +0900

    Update item.rb

--- spec/factories/item.rb
+++ spec/factories/item.rb
@@ -7,8 +7,9 @@ FactoryBot.define do
     condition_id          {"3"}
     item_explanation      {"ああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ"}
     preparationday_id     {"3"}
-    postagepayer_id          {"3"}
-    postagetype_id          {"3"}
+    postagepayer_id       {"3"}
+    postagetype_id        {"3"}
+    image               { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/redstyakiA033_TP_V.jpg')) }
   end
 
 end
