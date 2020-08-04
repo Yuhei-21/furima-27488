56c4541298960b74ccb53094c02bf33b62deeac2
Yuhei-21 <67134329+Yuhei-21@users.noreply.github.com>
2020-08-04 12:55:05 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,5 +9,6 @@ class Item < ApplicationRecord
 
   validates_inclusion_of :price, in: 300..100000000
   validates :image_presence
-  alidates :name, :text, :category_id, :condition_id, :preparationday_id, :postagetype_id, :postagepayer_id,:user_id, presence: true
+  validates :name, :text, :category_id, :condition_id, :preparationday_id, :postagetype_id, :postagepayer_id,:user_id, presence: true
+  validates :category_id, :condition_id, :preparationday_id, :postagetype_id, :postagepayer_id,:user_id, exclusion: { in: %w(1) }
 end
