9404afc54384ea3dd5078d786766ccea573066dd
Yuhei-21 <67134329+Yuhei-21@users.noreply.github.com>
2020-08-04 15:44:20 +0900

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,26 +7,26 @@ describe User do
     end
     # 1. nicknameが空では登録できないこと
     it "is invalid without a nickname" do
-      user.valid?
-      expect(user.errors[:nickname]).to include("can't be blank")
+      @user.valid?
+      expect(@user.errors[:nickname]).to include("can't be blank")
     end
 
     # 2. emailが空では登録できないこと
     it "is invalid without a email" do
-      user.valid?
-      expect(user.errors[:email]).to include("can't be blank")
+      @user.valid?
+      expect(@user.errors[:email]).to include("can't be blank")
     end
 
     # 3. passwordが空では登録できないこと
     it "is invalid without a password" do
-      user.valid?
-      expect(user.errors[:password]).to include("can't be blank")
+      @user.valid?
+      expect(@user.errors[:password]).to include("can't be blank")
     end
 
     # 4. passwordが存在してもpassword_confirmationが空では登録できないこと
     it "is invalid without a password_confirmation although with a password" do
-      user.valid?
-      expect(user.errors[:password_confirmation]).to include("doesn't match Password")
+      @user.valid?
+      expect(@user.errors[:password_confirmation]).to include("doesn't match Password")
     end
 
     # 5. 重複したemailが存在する場合登録できないこと
@@ -37,13 +37,13 @@ describe User do
 
     # 6. passwordが6文字以上であれば登録できること
     it "is valid with a password that has more than 6 characters " do
-      expect(user).to be_valid
+      expect(@user).to be_valid
     end
 
     # 7. passwordが5文字以下であれば登録できないこと
     it "is invalid with a password that has less than 5 characters " do
-      user.valid?
-      expect(user.errors[:password]).to include("is too short (minimum is 6 characters)")
+      @user.valid?
+      expect(@user.errors[:password]).to include("is too short (minimum is 6 characters)")
     end
   end
 end
